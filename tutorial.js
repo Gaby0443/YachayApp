626e8eebf23b8a5e1e8ab4a9f710dafc1e092d13
Gabriela Tirado <gt8263257@gmail.com>
2025-12-03 05:40:23 +0000

    commit

--- tutorial.js
+++ tutorial.js
@@ -2,3 +2,14 @@ function finalizarTutorial() {
   localStorage.setItem("tutorialCompletado", "true");
   location.href = "progreso.html";
 }
+
+function omitirTutorial() {
+  // Marcar como “completado” solo para efectos de mostrar la frase
+  localStorage.setItem("tutorialCompletado", "true");
+
+  // Nos aseguramos de que la frase pueda mostrarse (por si ya se limpió antes)
+  localStorage.removeItem("fraseMostrada");
+
+  // Redirigir a Progreso (donde está el modal de frase del día)
+  window.location.href = "progreso.html";
+}
